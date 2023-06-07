ac373e2db27d2546da31f32b5e3b2550168945f7
svyturys <tarento.sv@gmail.com>
2023-06-07 20:27:31 +0300

    0

---
+++ src/js/01-gallery.js
@@ -0,0 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryRef = document.querySelector('.gallery');
+
+// Creating Markup
+const markupForTaks = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+}).join('');
+
+// adding markup to the page
+
+galleryRef.innerHTML = markupForTaks;
+
+// Creating a modal
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+ });
